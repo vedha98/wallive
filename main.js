2f75eeb0e3e0850150cc4355de563ac152f0b67b
vedha gnanam <vetha.gnanam98@gmail.com>
2019-01-09 21:45:04 +0530

    created a file for handling windows

--- main.js
+++ main.js
@@ -1,27 +1,12 @@
 const electron = require('electron');
 const wallpaper = require('electron-wallpaper')
 const { app, BrowserWindow } = require('electron')
+const wincre = require('./wincre.js');
 let win
 
 function createWindow () {
-  const windowProperties = {
-              width: 1920,
-              height: 1080,
-              transparent: true,
-              frame: false
-            };
-win = new BrowserWindow({width: 1920,
-height: 1080})
+wincre.createMain();
 
-  win.loadFile('index.html')
-
-
-  win.webContents.openDevTools()
-
-
-  win.on('closed', () => {
-    win = null
-  })
 }
 
 
--- wincre.js
+++ wincre.js
@@ -1,17 +1,67 @@
 const electron = require('electron');
 const wallpaper = require('electron-wallpaper')
-const { app, BrowserWindow } = require('electron')
-function apply(){
+const { app,BrowserWindow} = require('electron')
+
+let win1,win,child
+function apply(param){
+  const Brows = electron.remote.BrowserWindow
   const windowProperties = {
               width: 1920,
               height: 1080,
               transparent: true,
               frame: false
             };
-  win1 = new BrowserWindow(windowProperties)
+            if(!win1){
+
+            win1 = new Brows(windowProperties)
+            }
+
 
   wallpaper.attachWindow(win1);
-  win1.loadFile('window1.html')
+  switch(param){
+case "heart":
+  win1.loadFile('window1.html');
+  break;
+  case "bandit":
+    win1.loadFile('bandit.html');
+      break;
+  default:
+  break;
+  }
+
 
 
 }
+function detatch(){
+if(win1){
+  win1.close();
+  win1=null;
+}
+
+}
+
+function createCustom(){
+hideMain();
+showCustom();
+}
+function hideMain() {
+  var main = document.getElementById("main")
+  main.style.display='none';
+}
+function showCustom() {
+
+}
+exports.createMain = function () {
+  win = new BrowserWindow({width: 1920,
+  height: 1080})
+
+    win.loadFile('index.html')
+
+
+    win.webContents.openDevTools()
+
+
+    win.on('closed', () => {
+      win = null
+    })
+};
