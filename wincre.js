2c30aae4e8cac7c52d11d2b5702c884ade07e228
vedha gnanam <vetha.gnanam98@gmail.com>
2019-01-10 07:38:44 +0530

    ui improvements
    added a splash screen
    updated ui
    added toast

--- wincre.js
+++ wincre.js
@@ -29,8 +29,7 @@ case "heart":
   default:
   break;
   }
-
-
+showToast('Wallpaper Set');
 
 }
 function detatch(){
@@ -38,7 +37,7 @@ if(win1){
   win1.close();
   win1=null;
 }
-
+showToast('wallpaper removed');
 }
 
 function createCustom(){
@@ -52,16 +51,41 @@ function hideMain() {
 function showCustom() {
 
 }
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
 exports.createMain = function () {
-  win = new BrowserWindow({width: 1920,
-  height: 1080,icon: 'icon.png'})
+  splash = new BrowserWindow({width: 1920,
+  height: 1080,icon: 'icon.png',frame:false,transparent:true})
+  splash.loadFile('splash.html')
+  setTimeout(function () {
+    win = new BrowserWindow({width: 1920,
+    height: 1080,icon: 'icon.png'})
+    splash.close();
+      win.loadFile('index.html')
+  win.setMenu(null);
+  }, 2000);
 
-    win.loadFile('index.html')
-win.setMenu(null);
 
 
-
-    win.on('closed', () => {
-      win = null
-    })
 };
+
+function showToast(text){
+  var toast =  document.getElementById("toastbox");
+  var toasttext = document.getElementById('toasttext');
+  toasttext.innerHTML=text;
+  toast.classList.add('show-toast');
+  toast.classList.remove('hide-toast');
+  setTimeout(function () {
+    toast.classList.add('hide-toast');
+    toast.classList.remove('show-toast');
+  }, 2000);
+
+
+
+
+}
